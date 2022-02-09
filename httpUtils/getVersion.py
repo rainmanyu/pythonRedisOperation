322f972b88e1f363b57c66c0d041afc0fae2b520
rainman.yu <rainman.yu@everymatrix.com>
2022-02-09 05:49:00 +0000

    add logsadd logs

--- httpUtils/getVersion.py
+++ httpUtils/getVersion.py
@@ -7,13 +7,13 @@ import logging
 def http_get(url, path):
     try:
         if not url:
+            return json.loads('{"error":"OtherError"}')
+        else:
             conn = http.client.HTTPSConnection(url)
             conn.request("GET", path)
             r1 = conn.getresponse()
             data1 = r1.read()  # This will return entire content.
             return json.loads(data1)
-        else:
-            return json.loads('{"error":"OtherError"}')
     except ConnectionRefusedError:
         print(url + path)
         return json.loads('{"error":"ConnectionRefusedError"}')
